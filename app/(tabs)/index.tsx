c8610d267b05384cbdb735605fae5fd294b987fb
Mihir Patankar <mpatankar06@gmail.com>
2024-11-22 15:46:40 -0500

    Revert index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,68 +1,12 @@
-import { StyleSheet, TextInput } from 'react-native';
-import { useState } from 'react';
+import SessionControls from '@/components/SessionControls';
 import { Text, View } from '@/components/Themed';
-import { Picker } from '@react-native-picker/picker';
+import { StyleSheet } from 'react-native';
 
 export default function HomeScreen() {
-  const [feet, setFeet] = useState('');
-  const [inches, setInches] = useState('');
-  const [weight, setWeight] = useState('');
-  const [gender, setGender] = useState('');
-
   return (
-    <View style={styles.itemAlign}>
-      <View style={styles.container}>
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Height</Text>
-          <View style={styles.heightContainer}>
-            <View style={styles.heightInput}>
-              <TextInput
-                style={styles.unitText}
-                value={feet}
-                onChangeText={setFeet}
-                placeholder="Feet"
-                placeholderTextColor="#666"
-                keyboardType="numeric"
-              />
-              <Text style={styles.unitText}>ft</Text>
-            </View>
-            <View style={styles.heightInput}>
-              <TextInput
-                style={styles.unitText}
-                value={inches}
-                onChangeText={setInches}
-                placeholder="Inches"
-                placeholderTextColor="#666"
-                keyboardType="numeric"
-              />
-              <Text style={styles.unitText}>in</Text>
-            </View>
-          </View>
-        </View>
-
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Weight (lbs)</Text>
-          <TextInput
-            style={styles.unitText}
-            value={weight}
-            onChangeText={setWeight}
-            placeholder="Weight"
-            placeholderTextColor="#666"
-            keyboardType="numeric"
-          />
-        </View>
-
-        <View style={styles.formGroup}>
-          <Text style={styles.label}>Gender</Text>
-          <Picker selectedValue={gender} onValueChange={(value) => setGender(value)} style={styles.picker}>
-            <Picker.Item label="Select gender" value="" />
-            <Picker.Item label="Male" value="male" />
-            <Picker.Item label="Female" value="female" />
-            <Picker.Item label="Non-binary" value="non-binary" />
-            <Picker.Item label="Prefer not to say" value="prefer not to say" />
-          </Picker>
-        </View>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Shot Clock</Text>
+      <SessionControls />
     </View>
   );
 }
@@ -70,45 +14,12 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
-    maxWidth: 500,
-  },
-  formGroup: {
-    marginBottom: 70,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 8,
-    color: 'white',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    padding: 8,
-    fontSize: 16,
-    color: 'white',
-  },
-  heightContainer: {
-    flexDirection: 'row',
-    gap: 12,
-    padding: 10,
-  },
-  heightInput: {
-    flexDirection: 'row',
     alignItems: 'center',
-    gap: 4,
-    flex: 1,
+    justifyContent: 'center',
+    rowGap: 100,
   },
-  picker: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-  },
-  unitText: {
-    color: 'white',
-  },
-  itemAlign: {
-    alignItems: 'center',
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
